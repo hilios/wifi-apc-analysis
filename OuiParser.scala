bc6d20db2406a55ecb9f041b5e73bd7a44bf4758
Edson Hilios <edson.hilios@gmail.com>
2017-07-25 22:41:10 -0300

    Improve oui parser and generate a new phone dataset

--- OuiParser.scala
+++ OuiParser.scala
@@ -1,15 +1,17 @@
 spark.conf.set("spark.sql.crossJoin.enabled", true)
+import org.apache.spark.sql._
+
+val basePath = "/Users/hilios/Sites/wifi-apc-analysis/data/oui"
 
 val p = """^([0-9A-F]{2}-[0-9A-F]{2}-[0-9A-F]{2})\s{1,}\(hex\)\t\t(.*)$""".r
-val oui = sc.textFile("/Users/hilios/Downloads/oui.txt").map {
+val oui = sc.textFile(s"$basePath/oui.txt").map {
   case p(prefix, name) => Some((prefix.replaceAll("-", ":"), name))
   case _ => None
 }.filter(_.nonEmpty).map(_.get).toDF("prefix",  "name")
 
-oui.coalesce(1).write.csv("/Users/hilios/Documents/oui-parsed.csv")
+oui.coalesce(1).write.csv(s"$basePath/oui-parsed.csv")
 
-val manufactures = sc.textFile("/Users/hilios/Downloads/mobile-manufactures.txt")
-  .filter(_.nonEmpty).toDF("manufacturer")
+val manufactures = spark.read.csv(s"$basePath/manufactures.csv").toDF("manufacturer", "count")
 
-val phones = manufactures.join(oui, $"name".startsWith($"manufacturer")).select("prefix", "name")
-phones.coalesce(1).write.csv("/Users/hilios/Documents/phones.csv")
+val phones = manufactures.join(oui, upper($"name").contains($"manufacturer") || lower($"name").contains("mobile")).select("prefix", "name").distinct()
+phones.coalesce(1).write.csv(s"$basePath/phones.csv")
